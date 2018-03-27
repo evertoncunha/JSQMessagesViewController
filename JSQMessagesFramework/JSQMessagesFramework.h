9c66a92777affeb83421d449e2b2d561b27c9dd4
Everton Cunha <ever.cunha@gmail.com>
2018-03-27 16:28:21 -0300

    cherry

---
+++ JSQMessagesFramework/JSQMessagesFramework.h
@@ -0,0 +1,64 @@
+//
+//  JSQMessagesFramework.h
+//  JSQMessagesFramework
+//
+//  Created by Everton Cunha on 27/03/2018.
+//  Copyright © 2018 Hexed Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSQMessagesFramework.
+FOUNDATION_EXPORT double JSQMessagesFrameworkVersionNumber;
+
+//! Project version string for JSQMessagesFramework.
+FOUNDATION_EXPORT const unsigned char JSQMessagesFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSQMessagesFramework/PublicHeader.h>
+
+
+
+#import "NSBundle+JSQMessages.h"
+#import "NSString+JSQMessages.h"
+#import "UIColor+JSQMessages.h"
+#import "UIImage+JSQMessages.h"
+#import "JSQMessagesAvatarImageFactory.h"
+#import "JSQMessagesBubbleImageFactory.h"
+#import "JSQMessagesMediaViewBubbleImageMasker.h"
+#import "JSQMessagesTimestampFormatter.h"
+#import "JSQMessagesToolbarButtonFactory.h"
+#import "JSQMessagesVideoThumbnailFactory.h"
+#import "JSQAudioMediaViewAttributes.h"
+#import "JSQMessagesBubbleSizeCalculating.h"
+#import "JSQMessagesBubblesSizeCalculator.h"
+#import "JSQMessagesCollectionViewFlowLayout.h"
+#import "JSQMessagesCollectionViewFlowLayoutInvalidationContext.h"
+#import "JSQMessagesCollectionViewLayoutAttributes.h"
+#import "JSQAudioMediaItem.h"
+#import "JSQLocationMediaItem.h"
+#import "JSQMediaItem.h"
+#import "JSQMessage.h"
+#import "JSQMessageAvatarImageDataSource.h"
+#import "JSQMessageBubbleImageDataSource.h"
+#import "JSQMessageData.h"
+#import "JSQMessageMediaData.h"
+#import "JSQMessagesAvatarImage.h"
+#import "JSQMessagesBubbleImage.h"
+#import "JSQMessagesCollectionViewDataSource.h"
+#import "JSQMessagesCollectionViewDelegateFlowLayout.h"
+#import "JSQPhotoMediaItem.h"
+#import "JSQVideoMediaItem.h"
+#import "JSQMessagesViewAccessoryButtonDelegate.h"
+#import "JSQMessagesCellTextView.h"
+#import "JSQMessagesCollectionView.h"
+#import "JSQMessagesCollectionViewCell.h"
+#import "JSQMessagesCollectionViewCellIncoming.h"
+#import "JSQMessagesCollectionViewCellOutgoing.h"
+#import "JSQMessagesComposerTextView.h"
+#import "JSQMessagesInputToolbar.h"
+#import "JSQMessagesLabel.h"
+#import "JSQMessagesLoadEarlierHeaderView.h"
+#import "JSQMessagesMediaPlaceholderView.h"
+#import "JSQMessagesToolbarContentView.h"
+#import "JSQMessagesTypingIndicatorFooterView.h"
+#import "JSQMessagesTypingView.h"
